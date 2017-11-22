f45250635ac5a8dd6f865d13f046470261bef5f8
Shyam Valsan <shyam.valsan@gmail.com>
2017-11-23 01:51:45 +0530

    update slopegraph code

--- R/SlopeGraph.R
+++ R/SlopeGraph.R
@@ -8,7 +8,6 @@ y.locs<- c(0,1,5)
 x[,1]<-as.character(x[,1]) 
 listOfWards<-ncol(x)-1
 ranking<-2:ncol(x)
-rankingOriginal<-x[,ranking]
 x[,ranking]<-rescale(as.matrix(x[,ranking]),c(5,25))  #rescale so line sizes make sense
 
 #reorder column ranking 
@@ -17,60 +16,56 @@ x[,ranking]<-x[,ranking][,first.ord]
 names(x)[ranking]<-names(x)[ranking][first.ord]
 
 #list population of wards in decreasing order for each date
-data_col<-vector('list',nrow(x))
-names(data_col)<-x[,1]
+columns<-vector('list',nrow(x))
+names(columns)<-x[,1]
 for(val in 1:nrow(x)){
   tmp<-t(x[val,ranking])
   tmp<-tmp[order(tmp,decreasing=T),,drop=F]
-  data_col[[val]]<-tmp
+  columns[[val]]<-tmp
 }
 
 #initiate plot object
-plot(dim.x,dim.y,type='n',axes=F,xlab='',ylab='') 
+plot(c(0,1),c(0,1),type='n',axes=F,xlab='',ylab='') 
 
 #x locations
-x.vals<-rep(seq(x.locs[1],x.locs[2],length=length(data_col)),each=listOfWards)
+x.vals<-rep(seq(x.locs[1],x.locs[2],length=length(columns)),each=listOfWards)
 x.vals<-split(x.vals,x.vals)
 
 #y locations, rearranged in loop, exception if dates are plotted
 y.vals<-rev(seq(y.locs[1],y.locs[2],length=listOfWards+1))[-1]
 
 #get line colors  ``  
-line.cols<-alpha(colorRampPalette(c("red","yellow","green"))(listOfWards),0.7)
+line_colors<-alpha(colorRampPalette(c("red","yellow","green"))(listOfWards),0.7)
 
-for(val in 1:(length(data_col)-1)){
+for(val in 1:(length(columns)-1)){
   
   #temp data to plot
-  plt.tmp<-data_col[c(val,val+1)]
-  x.tmp<-x.vals[c(val,val+1)]
+  temp_plot<-columns[c(val,val+1)]
+  x_tmp<-x.vals[c(val,val+1)]
   
   #plot temp text for column, remove spp if rnks
-  rowtxt <- row.names(plt.tmp[[1]])
-  text(x.tmp[[1]],y.vals,rowtxt)
+  rowtxt <- row.names(temp_plot[[1]])
+  text(x_tmp[[1]],y.vals,rowtxt)
   
-  if(val == length(data_col)-1){
-    rowtxt <- row.names(plt.tmp[[2]])
-    text(x.tmp[[2]],y.vals,rowtxt)
-    dt.txt<-substitute(bold(x),list(x=names(plt.tmp)[2]))
-    text(unique(x.tmp[[2]]),y.locs[2],dt.txt)
+  if(val == length(columns)-1){
+    rowtxt <- row.names(temp_plot[[2]])
+    text(x_tmp[[2]],y.vals,rowtxt)
+    heading<-substitute(bold(x),list(x=names(temp_plot)[2]))
+    text(unique(x_tmp[[2]]),y.locs[2],heading)
   }	
   
-  dt.txt<-substitute(bold(x),list(x=names(plt.tmp)[1]))  
-  text(unique(x.tmp[[1]]),y.locs[2],dt.txt)
+  heading<-substitute(bold(x),list(x=names(temp_plot)[1]))  
+  text(unique(x_tmp[[1]]),y.locs[2],heading)
   
-  srt.ln.y<-match(row.names(plt.tmp[[1]]),row.names(plt.tmp[[2]]))
+  sort_names<-match(row.names(temp_plot[[1]]),row.names(temp_plot[[2]]))
   
   #if no line rescale, use first element of rs.ln
-  lwd.val<-plt.tmp[[1]][,1]
-  
-  #vector for species selection of line segments
-  sel.sp<-rep(T,listOfWards)
+  line_width<-temp_plot[[1]][,1]
   
   #for lines
-  segments(x.tmp[[1]][sel.sp], y.vals[sel.sp], x.tmp[[2]][sel.sp],
-    y.vals[srt.ln.y][sel.sp], col=line.cols[sel.sp], lwd=lwd.val[sel.sp])
+  segments(x_tmp[[1]], y.vals, x_tmp[[2]], y.vals[sort_names], col=line_colors, lwd=line_width)
   
   #resort color vector for next column
-  srt.cl.y<-match(row.names(plt.tmp[[2]]),row.names(plt.tmp[[1]]))
-  line.cols<-line.cols[srt.cl.y]
+  sort_colors<-match(row.names(temp_plot[[2]]),row.names(temp_plot[[1]]))
+  line_colors<-line_colors[sort_colors]
 }
